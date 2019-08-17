147e54ce07dd9dd8eafc27b99e7fe79417422ebb
Egor Glushkov <egorglushkov2014@yandex.ru>
2019-08-17 06:16:58 +0300

    comparative stats

--- parsing and plotting stat/Comparative_stats.py
+++ parsing and plotting stat/Comparative_stats.py
@@ -1,19 +1,36 @@
 import re
 import csv
-from matplotlib import pyplot as plt, dates as mdates, colors as mclrs
+from matplotlib import pyplot as plt, dates as mdates, colors as mclrs, ticker
 from datetime import datetime, timedelta
 import numpy as np
 import random
 
+nick_name_list_to_compare = ["PeX", "ad", "Axaris", "Fexa"]
+
 
 def save_figure(fig, name):
     fig.set_size_inches((11.69, 8.27), forward=False)  # A4
-    fig.savefig("output_data/" + name + ".pdf", dpi=1000)
+    fig.savefig("output_data_comparative_stat/" + name + ".pdf", dpi=1000)
 
 
-def set_ax_with_personal_stats(chosen_nick, ax, clr):
+def set_bar_axes(axes_list, x_labels, bar_data_list, title_list):
+    color_list = np.random.rand(len(nick_list), 3)  # RGB
+    for rank, ax in enumerate(axes_list):
+        ax.bar(x_labels, bar_data_list[rank], align='center', color=color_list)
+        ax.set_title(title_list[rank])
+        if len(x_labels) >= 4:
+            ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+    # for total points
+    axes_list[1].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.1f}'.format(x / 1000) + 'K'))
+
+
+def set_ax_with_personal_stats(chosen_nick, ax, clr, clr2, mrkr):
     time_counter = 0
-    date_points_map_dict = {}
+    date_points_map_dict = {}   # date_points_map_dict is main program structure
+    # keys are every date (day) in race file
+    # values are [0, set(finished maps on this day)]
+    # later it will transform to [points_counted_by_finished_on_this_day_maps, set(...)]
+    # and later to [points (only for this day), current_number_of_points (all points before + this day)]
     set_of_finished_maps = set()
     with open('../ddnet-stats/race.csv', 'r', encoding='utf-8') as f:
         next(f)  # skipping header
@@ -28,15 +45,15 @@ def set_ax_with_personal_stats(chosen_nick, ax, clr):
                     timestamp = datetime.date(datetime.strptime((other_info[-2])[1:-1], "%Y-%m-%d %H:%M:%S"))
                     if timestamp not in date_points_map_dict.keys():
                         date_points_map_dict[timestamp] = [0, []]
-                    date_points_map_dict[timestamp][1].append(finished_map)
-                    set_of_finished_maps.add(finished_map)
+                    date_points_map_dict[timestamp][1].append(finished_map)  # making set of finished maps on this day
+                    set_of_finished_maps.add(finished_map)  # only for map_counter
 
     map_counter = len(set_of_finished_maps)  # total count of different finished maps
     total_points = 0
     for key in date_points_map_dict.keys():
         for map in date_points_map_dict[key][1]:
-            date_points_map_dict[key][0] += map_points[map]
-        total_points += date_points_map_dict[key][0]
+            date_points_map_dict[key][0] += map_points[map]  # counting points for all maps on this day
+        total_points += date_points_map_dict[key][0]    # counting points for all dates
 
     # list(el) because we need to transform tuple to list (to change, tuple is unchangeable)
     top5_days_max_points = [list(el) for el in
@@ -53,7 +70,7 @@ def set_ax_with_personal_stats(chosen_nick, ax, clr):
     current_points = 0
     for k in date_points_map_dict.keys():
         current_points += date_points_map_dict[k][0]
-        date_points_map_dict[k][1] = current_points
+        date_points_map_dict[k][1] = current_points  # writing count of points for less or equal this date
 
     first_date = list(date_points_map_dict.items())[0][0]
     last_date = list(date_points_map_dict.items())[-1][0]
@@ -66,6 +83,10 @@ def set_ax_with_personal_stats(chosen_nick, ax, clr):
         if short_date not in monthly_stat.keys():
             monthly_stat[short_date] = [0, 0]
         curr_date += timedelta(days=27)
+    # curr date could be more than last date but last month 'd not be in dict
+    short_date = last_date.strftime("%Y-%m")
+    if short_date not in monthly_stat.keys():
+        monthly_stat[short_date] = [0, 0]
 
     for k in date_points_map_dict.keys():
         monthly_stat[k.strftime("%Y-%m")][0] += date_points_map_dict[k][0]
@@ -100,33 +121,40 @@ def set_ax_with_personal_stats(chosen_nick, ax, clr):
 
     ax[0].plot_date(date_points_map_dict.keys(), [date_points_map_dict[k][1] for k in date_points_map_dict.keys()],
                     fmt='.', c=clr, label=chosen_nick)
-    ax[0].plot_date(dates_max_points_list, totally_max_points_list, c=clr, fmt='x', label=chosen_nick)
+    ax[0].plot_date(dates_max_points_list, totally_max_points_list, c=clr2, fmt='x', label=chosen_nick)
 
     ax[1].plot_date(date_points_map_dict.keys(), [date_points_map_dict[k][0] for k in date_points_map_dict.keys()],
                     fmt='.', c=clr, label=chosen_nick)
-    ax[1].plot_date(dates_max_points_list, daily_max_points_list, c=clr, fmt='x', label=chosen_nick)
+    ax[1].plot_date(dates_max_points_list, daily_max_points_list, c=clr2, fmt='x', label=chosen_nick)
 
     months_list = [datetime.strptime(k, "%Y-%m") for k in monthly_stat.keys()]
-    ax[2].plot(months_list, [monthly_stat[k][1] for k in monthly_stat.keys()], c=clr, label=chosen_nick)
-    ax[2].plot(months_list, [monthly_stat[k][1] for k in monthly_stat.keys()], c=clr, marker='.', markeredgecolor='k')
-    ax[3].plot_date(months_list, [monthly_stat[k][0] for k in monthly_stat.keys()], c=clr, marker='.', markeredgecolor='k', label=chosen_nick)
+    ax[2].plot(months_list, [monthly_stat[k][1] for k in monthly_stat.keys()], c=clr)
+    ax[2].plot(months_list, [monthly_stat[k][1] for k in monthly_stat.keys()], c=clr, marker='.', markeredgecolor='k', label=chosen_nick)
+    ax[3].plot_date(months_list, [monthly_stat[k][0] for k in monthly_stat.keys()], c=clr, marker=mrkr, markeredgecolor='k', label=chosen_nick)
 
     annual_list = [datetime.strptime(k, "%Y") for k in annual_stat.keys()]
     ax[4].plot(annual_list, [annual_stat[k][1] for k in annual_stat.keys()], c=clr)
     ax[4].plot(annual_list, [annual_stat[k][1] for k in annual_stat.keys()], c=clr, marker='^', markeredgecolor='k', label=chosen_nick)
-    ax[5].scatter(annual_list, [annual_stat[k][0] for k in annual_stat.keys()], label=chosen_nick, c=clr, edgecolor='k', marker='^')
+    ax[5].scatter(annual_list, [annual_stat[k][0] for k in annual_stat.keys()], label=chosen_nick, c=clr, edgecolor='k', marker=mrkr)
     return [total_time, total_points, max_points_per_1_day, map_counter]
 
 
 def set_axes_for_comp_stats(nickname_list, axes_list):
+    # to choose different random colors and marker for each player
+    n = len(nickname_list)
     clr_list = list(mclrs.cnames.keys())
+    marker_list = [".", "o", "v", "^", "<", ">", "1", "2", "s", "p", "P", "*", "h", "H", "+", "x", "X", "D", "d"]
     rand_color_list = []
-    for i in range(len(nickname_list)):
+    rand_marker_list = []
+    for i in range(n):
         rand_color_list.append(clr_list[random.randint(0, len(clr_list) - 1)])
-    print(rand_color_list)
+        rand_color_list.append(clr_list[random.randint(0, len(clr_list) - 1)])
+        rand_marker_list.append(marker_list[random.randint(0, len(marker_list) - 1)])
+
     info = []
     for num, nickname in enumerate(nickname_list):
-        info.append(set_ax_with_personal_stats(nickname, axes_list, rand_color_list[num]))
+        info.append(set_ax_with_personal_stats(nickname, axes_list, rand_color_list[num],
+                                               rand_color_list[num+n], rand_marker_list[num]))
     return info
 
 
@@ -158,7 +186,8 @@ ax22.xaxis.set_major_locator(mdates.RRuleLocator(rule))
 ax22.xaxis.set_major_formatter(formatter=mdates.DateFormatter('%Y-%m'))
 ax22.xaxis.set_tick_params(rotation=75, labelsize=9)
 
-nick_list = ["PeX", "ad", "Axaris"]
+nick_list = nick_name_list_to_compare
+
 personal_info = set_axes_for_comp_stats(nick_list, [ax11, ax12, ax21, ax22, ax31, ax32])
 
 ax11.set_title("Total points")
@@ -171,7 +200,8 @@ ax12.set_ylabel('Points per day')
 ax12.grid()
 ax12.legend()
 fig1.tight_layout()
-# save_figure(fig1, "daily_stats")
+
+save_figure(fig1, "daily_stats")
 
 ax21.set_title("Monthly total points")
 ax21.set_ylabel('Total points')
@@ -183,7 +213,8 @@ ax22.set_ylabel('Points per month')
 ax22.legend()
 ax22.grid()
 fig2.tight_layout()
-# save_figure(fig2, "monthly_stats")
+
+save_figure(fig2, "monthly_stats")
 
 ax31.set_title("Annual total points")
 ax31.set_ylabel('Total points')
@@ -195,28 +226,20 @@ ax32.set_ylabel('Points per year')
 ax32.legend()
 ax32.grid()
 fig3.tight_layout()
-# save_figure(fig3, "annual_stats")
 
-fig_bar, (ax_bar1, ax_bar2, ax_bar3, ax_bar4) = plt.subplots(nrows=1, ncols=4)
+save_figure(fig3, "annual_stats")
 
-# personal_info[i] = [total_time, total_points, max_points_per_1_day, map_counter]
-bar_data_total_time = [item[0] for item in personal_info]
-bar_data_total_points = [item[1] for item in personal_info]
-bar_data_max_points = [item[2] for item in personal_info]
-bar_data_map_counter = [item[3] for item in personal_info]
+fig_bar, ax_bar = plt.subplots(nrows=1, ncols=4)
 
-color_list = np.random.rand(len(nick_list), 3)  # RGB
+titles = ['Total time of all finishes (in hours)', 'Total points', 'Max points per day', 'Count of finished maps']
+bar_data = [
+    [item[0] for item in personal_info],    # total_time
+    [item[1] for item in personal_info],    # total_points
+    [item[2] for item in personal_info],    # max_points_per_1_day
+    [item[3] for item in personal_info]     # map_counter
+]
 
-ax_bar1.bar(nick_list, bar_data_total_time, align='center', color=color_list)
-ax_bar1.set_title('Total time of all finishes (in hours)')
+set_bar_axes(ax_bar, nick_list, bar_data, titles)
 
-ax_bar2.bar(nick_list, bar_data_total_points, align='center', color=color_list)
-ax_bar2.set_title('Total points')
-
-ax_bar3.bar(nick_list, bar_data_max_points, align='center', color=color_list)
-ax_bar3.set_title('Max points per day')
-
-ax_bar4.bar(nick_list, bar_data_map_counter, align='center', color=color_list)
-ax_bar4.set_title('Count of finished maps')
-
-plt.show()
+save_figure(fig_bar, "personal_stat")
+# plt.show()
