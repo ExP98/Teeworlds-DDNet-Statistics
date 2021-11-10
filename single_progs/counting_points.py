fb12b49e13b78f133ee78fbc887d1cc251bd3143
Egor Glushkov <egorglushkov2014@yandex.ru>
2021-11-10 09:49:29 +0300

    counting points & total time

--- single_progs/counting_points.py
+++ single_progs/counting_points.py
@@ -1,48 +1,41 @@
 import re
 import csv
+from collections import defaultdict
 
 # dict {'map':points}
 map_points = {}
-with open('../ddnet-stats/maps.csv', 'r', encoding='utf-8') as fp:
+with open('../ddnet-stats/maps.csv', 'r', encoding='latin-1') as fp:
     reader = csv.reader(fp, delimiter=',')
     next(fp)
     for row in reader:
         map_points[row[0]] = int(row[2])
 
-# for deleted maps
-with open('../ddnet-stats/race.csv', 'r', encoding='utf-8') as fp:
-    reader = csv.reader(fp, delimiter=',')
-    for row in reader:
-        if row[0] not in map_points.keys():
-            map_points[row[0]] = 0
-
-nick_dict = {}
-
-with open('../ddnet-stats/race.csv', 'r', encoding='utf-8') as f:
+nick_dict = defaultdict(set)
+with open('../ddnet-stats/race.csv', 'r', encoding='latin-1') as f:
     next(f)  # skipping header
     for line in f:
         map = re.split(',".*",', line)[0][1:-1]
         nick = ','.join((re.findall(',".*",', line))[0].split(',')[1:-3])[1:-1]
-        if nick not in nick_dict.keys():
-            nick_dict[nick] = {map}
-        else:
-            nick_dict[nick].add(map)
+        nick_dict[nick].add(map)
+print('count of diff nicks: ', len(nick_dict.keys()))
 
+pts_dict = dict()
 for key in nick_dict.keys():
     total_points = 0
     for mp in nick_dict[key]:
         total_points += map_points[mp]
-    nick_dict[key] = total_points
-print('count of diff nicks: ', len(nick_dict.keys()))
+    if total_points >= 1000:
+        pts_dict[key] = total_points
 
-sorted_list_of_tuple_points = sorted(nick_dict.items(), key=lambda x: x[1], reverse=True)
+print('count of selected nicks: ', len(pts_dict.keys()))
 
-with open('../output_files/nicks-points.csv', 'w', encoding='utf-8') as f:
+sorted_list_of_tuple_points = sorted(pts_dict.items(), key=lambda x: x[1], reverse=True)
+
+with open('../output_files/nicks_points_1000pt.csv', 'w', encoding='latin-1') as f:
     f.write("Rank,Nickname,Points\n")
     for rank, (nick_name, points) in enumerate(sorted_list_of_tuple_points):
         f.write('%d,%s,%d\n' % (rank+1, nick_name, points))
 
-print("PeX", nick_dict["PeX"])
-print("Starkiller", nick_dict["Starkiller"])
-print("ad", nick_dict["ad"])
-print("not PeX", nick_dict["not PeX"])
+print("PeX", pts_dict["PeX"])
+print("Cor", pts_dict["Cor"])
+print("not PeX", pts_dict["not PeX"])
--- total_time/summary_time_for_players.py
+++ total_time/summary_time_for_players.py
@@ -17,7 +17,7 @@ sort_d = sorted(time_players_dict.items(), key=lambda x: x[1], reverse=True)
 for rank, (player, time) in enumerate(sort_d[0:15]):
     print(rank + 1, player, round(time/3600, 2))  # in hours
 
-with open('../output_files/sum_time_for_all.txt', 'w', encoding='latin-1') as f:
+with open('../output/sum_time_for_all.txt', 'w', encoding='latin-1') as f:
     f.write("Rank. Player: TotalTime(Hours)\n")
     for rank, (player, time) in enumerate(sort_d):
         f.write('%d. %s: %s\n' % (rank + 1, player, round(time/3600, 2)))
